728b8e7e12a27127516cfba17c67dca6284b7279
emily-gutierrez <67969028+emily-gutierrez@users.noreply.github.com>
2020-07-15 19:54:29 -0500

    Add files via upload

---
+++ calculator.playground/Contents.swift
@@ -0,0 +1,18 @@
+//Input
+var number1 = 1
+var number2 = 2
+var number3 = 3
+var number4 = 4
+var number5 = 5
+
+//operations
+2 * 2
+2 + 2
+2 / 2
+2 - 2
+
+//output
+print ( number4 - number1)
+print ( number3 * number5)
+print ( number2 / number1)
+print ( number5 + number2)
